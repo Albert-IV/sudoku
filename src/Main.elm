fabf902744a7a1a846598776c7faf32b82ef916b
Albert Engelbrecht <me@albertengelbrecht.com>
2019-12-26 16:55:12 -0600

    Added ability to update cell values.
    TODO: Next is to validate the changed cell value after changing and
    updating the background color if the value isn't valid.

--- src/Main.elm
+++ src/Main.elm
@@ -7,7 +7,6 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode exposing (Decoder, field, int, list)
-import Tuple exposing (..)
 
 
 
@@ -50,54 +49,39 @@ type Difficulty
 type alias SudokuCell =
     { y : Int
     , x : Int
-    , value : Int
+    , value : String
     , cellType : CellType
     }
 
 
 type CellType
     = StaticCell
-    | InputCell
-    | EmptyCell
+    | RegularCell
 
 
-getInitialGame : Array (Array SudokuCell)
-getInitialGame =
-    createSudokuFromLists
-        [ [ 0, 0, 8, 0, 0, 3, 0, 0, 2 ]
-        , [ 0, 4, 0, 5, 6, 0, 1, 0, 0 ]
-        , [ 0, 2, 6, 7, 0, 0, 0, 0, 4 ]
-        , [ 0, 5, 0, 0, 3, 0, 0, 2, 0 ]
-        , [ 0, 0, 0, 2, 8, 9, 0, 0, 0 ]
-        , [ 0, 8, 0, 0, 5, 0, 0, 9, 0 ]
-        , [ 2, 0, 0, 0, 0, 4, 8, 6, 0 ]
-        , [ 0, 0, 3, 0, 9, 5, 0, 7, 0 ]
-        , [ 8, 0, 0, 1, 0, 0, 3, 0, 0 ]
-        ]
-
-
-createSudokuFromLists : List (List Int) -> Array (Array SudokuCell)
+createSudokuFromLists : List (List String) -> Array (Array SudokuCell)
 createSudokuFromLists state =
-    Array.fromList (List.indexedMap convertSudokuRow state)
+    Array.fromList (List.indexedMap convertSudokuRowFromList state)
 
 
-convertSudokuRow : Int -> List Int -> Array SudokuCell
-convertSudokuRow y row =
-    let
-        convertCell x val =
-            case val of
-                0 ->
-                    { x = x, y = y, value = 0, cellType = EmptyCell }
+convertSudokuRowFromList : Int -> List String -> Array SudokuCell
+convertSudokuRowFromList y row =
+    Array.fromList (List.indexedMap (convertCell y) row)
 
-                _ ->
-                    { x = x, y = y, value = val, cellType = StaticCell }
-    in
-    Array.fromList (List.indexedMap convertCell row)
+
+convertCell : Int -> Int -> String -> SudokuCell
+convertCell y x val =
+    case val of
+        "0" ->
+            { x = x, y = y, value = "0", cellType = RegularCell }
+
+        _ ->
+            { x = x, y = y, value = val, cellType = StaticCell }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { game = getInitialGame, difficulty = Easy, gameStatus = Loading }, loadGame Easy )
+    ( { game = Array.fromList [ Array.fromList [] ], difficulty = Easy, gameStatus = Loading }, loadGame Easy )
 
 
 
@@ -105,17 +89,25 @@ init _ =
 
 
 type Msg
-    = MakeInput Int Int
-    | LoadGame
+    = LoadGame
     | LoadedBoard (Result Http.Error (List (List Int)))
     | DifficultyChanged Difficulty
+    | SetCellValue CellChangeEvent
+    | NoOp
+
+
+type alias CellChangeEvent =
+    { x : Int
+    , y : Int
+    , value : String
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        MakeInput y x ->
-            ( convertToStaticCell y x model, Cmd.none )
+        NoOp ->
+            ( model, Cmd.none )
 
         DifficultyChanged difficulty ->
             ( { model | difficulty = difficulty }, Cmd.none )
@@ -123,54 +115,23 @@ update msg model =
         LoadGame ->
             ( { model | gameStatus = Loading }, loadGame model.difficulty )
 
+        SetCellValue changeEvent ->
+            ( { model | game = setFromCoordinates changeEvent model.game }, Cmd.none )
+
         LoadedBoard result ->
             case result of
                 Ok newGame ->
                     let
+                        parsedResponse =
+                            List.map (\column -> List.map (\cell -> String.fromInt cell) column) newGame
+
                         game =
-                            createSudokuFromLists newGame
+                            createSudokuFromLists parsedResponse
                     in
                     ( { model | gameStatus = Loaded, game = game }, Cmd.none )
 
                 Err _ ->
                     ( { model | gameStatus = Error }, Cmd.none )
-
-
-loadGame : Difficulty -> Cmd Msg
-loadGame difficulty =
-    -- Currently using API from: https://github.com/berto/sugoku
-    -- Eventually we'll auto-create our own Sudoku puzzles, but for now...
-    Http.get
-        { url = "https://sugoku.herokuapp.com/board?difficulty=" ++ String.toLower (difficultyToString difficulty)
-        , expect = Http.expectJson LoadedBoard boardDecoder
-        }
-
-
-boardDecoder : Decoder (List (List Int))
-boardDecoder =
-    field "board" (list (list int))
-
-
-convertGameToList : Array (Array SudokuCell) -> List (List SudokuCell)
-convertGameToList game =
-    Array.toList (Array.map Array.toList game)
-
-
-convertToStaticCell : Int -> Int -> Model -> Model
-convertToStaticCell y x { game, difficulty } =
-    let
-        selectedRow =
-            Maybe.withDefault
-                (Array.initialize 9 (\n -> { x = n, y = y, value = n, cellType = StaticCell }))
-                (Array.get y game)
-
-        updatedRow =
-            Array.set x { cellType = StaticCell, value = 10, x = x, y = y } selectedRow
-
-        updatedGame =
-            Array.set y updatedRow game
-    in
-    { game = updatedGame, difficulty = difficulty, gameStatus = Loaded }
 
 
 
@@ -191,6 +152,74 @@ view model =
     { title = "SUDOKU!!!"
     , body = pageContents model
     }
+
+
+loadGame : Difficulty -> Cmd Msg
+loadGame difficulty =
+    -- Currently using API from: https://github.com/berto/sugoku
+    -- Eventually we'll auto-create our own Sudoku puzzles, but for now...
+    Http.get
+        -- { url = "" ++ String.toLower (difficultyToString difficulty)
+        { url = "https://sugoku.herokuapp.com/board?difficulty=" ++ String.toLower (difficultyToString difficulty)
+        , expect = Http.expectJson LoadedBoard boardDecoder
+        }
+
+
+boardDecoder : Decoder (List (List Int))
+boardDecoder =
+    field "board" (list (list int))
+
+
+setFromCoordinates : CellChangeEvent -> Array (Array SudokuCell) -> Array (Array SudokuCell)
+setFromCoordinates { y, x, value } listOfLists =
+    let
+        cellValue =
+            { y = y, x = x, value = value, cellType = RegularCell }
+
+        setInnerVal arr =
+            Array.set x cellValue arr
+
+        innerArr =
+            case Array.get y listOfLists of
+                Nothing ->
+                    Array.fromList []
+
+                Just val ->
+                    val
+    in
+    Array.set y (setInnerVal innerArr) listOfLists
+
+
+
+-- Eventually may be useful?
+-- getFromCoordinates : Int -> Int -> Array (Array SudokuCell) -> SudokuCell
+-- getFromCoordinates y x game =
+--     let
+--         defaultCellValue =
+--             { y = y
+--             , x = x
+--             , value = ""
+--             , cellType = RegularCell
+--             }
+--         outerVal =
+--             case Array.get y game of
+--                 Nothing ->
+--                     Array.fromList [ defaultCellValue ]
+--                 Just row ->
+--                     row
+--         innerVal =
+--             case Array.get x outerVal of
+--                 Nothing ->
+--                     defaultCellValue
+--                 Just cell ->
+--                     cell
+--     in
+--     innerVal
+
+
+convertGameToList : Array (Array t) -> List (List t)
+convertGameToList game =
+    Array.toList (Array.map Array.toList game)
 
 
 difficultyFromString : String -> Difficulty
@@ -243,9 +272,9 @@ difficultySelect model =
         ]
 
 
-changeDifficultyBtn : Html Msg
-changeDifficultyBtn =
-    button [ type_ "button", onClick LoadGame ] [ text "New Game" ]
+changeDifficultyBtn : GameStatus -> Html Msg
+changeDifficultyBtn status =
+    button [ type_ "button", onClick LoadGame, disabled (status == Loading) ] [ text "New Game" ]
 
 
 pageContents : Model -> List (Html Msg)
@@ -256,7 +285,7 @@ pageContents model =
             , h3 []
                 [ text "Difficulty: "
                 , difficultySelect model
-                , changeDifficultyBtn
+                , changeDifficultyBtn model.gameStatus
                 ]
             , getBoardElems model
             ]
@@ -275,8 +304,8 @@ getBoardElems model =
 
         Error ->
             div []
-                [ h6 [] [ text "Error loading game... defaulting to hard coded game.  Sorry!" ]
-                , displayGameBoard model
+                [ h6 [] [ text "Error loading game, try again maybe?" ]
+                , loadingMessage
                 ]
 
 
@@ -295,132 +324,94 @@ displayGameBoard model =
 displayRows : List (List SudokuCell) -> List (Html Msg)
 displayRows rows =
     let
-        displayRow rowIdx row =
-            div [ class "flex" ] (displayColumns row rowIdx)
+        displayRow row =
+            div [ class "flex" ] (displayColumns row)
     in
-    List.indexedMap displayRow rows
+    List.map displayRow rows
 
 
-displayColumns : List SudokuCell -> Int -> List (Html Msg)
-displayColumns columns rowIdx =
+displayColumns : List SudokuCell -> List (Html Msg)
+displayColumns columns =
     let
-        displayColumn x column =
-            case column.cellType of
-                StaticCell ->
-                    div [ class "cell flex static" ] [ text (String.fromInt column.value) ]
-
-                InputCell ->
-                    div [ class "cell flex", onClick (MakeInput rowIdx x) ] [ text (String.fromInt column.value) ]
-
-                EmptyCell ->
-                    div [ class "cell flex", onClick (MakeInput rowIdx x) ] [ text "" ]
+        displayColumn column =
+            displayCell column
 
         cellContents =
-            List.indexedMap displayColumn columns
+            List.map displayColumn columns
     in
     [ div [ class "flex" ] cellContents ]
 
 
-loadingMessage : Html Msg
-loadingMessage =
-    div [ class "board" ]
-        [ div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
+displayCell : SudokuCell -> Html Msg
+displayCell cell =
+    let
+        isLeft =
+            modBy 3 cell.x == 0
+
+        isRight =
+            modBy 3 cell.x == 2
+
+        isTop =
+            modBy 3 cell.y == 0
+
+        isBottom =
+            modBy 3 cell.y == 2
+
+        divContents =
+            [ input
+                [ class "cell-input"
+                , type_ "text"
+                , maxlength 1
+                , size 2
+                , readonly (cell.cellType == StaticCell)
+                , value
+                    (if cell.value == "0" then
+                        ""
+
+                     else
+                        cell.value
+                    )
+                , onInput (\val -> SetCellValue { y = cell.y, x = cell.x, value = val })
+                ]
+                []
             ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex static" ] [ text "L" ]
-            , div [ class "cell flex static" ] [ text "O" ]
-            , div [ class "cell flex static" ] [ text "A" ]
-            , div [ class "cell flex static" ] [ text "D" ]
-            , div [ class "cell flex static" ] [ text "I" ]
-            , div [ class "cell flex static" ] [ text "N" ]
-            , div [ class "cell flex static" ] [ text "G" ]
-            , div [ class "cell flex" ] []
-            ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            ]
-        , div [ class "flex" ]
-            [ div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
-            , div [ class "cell flex" ] []
+    in
+    div
+        [ classList
+            [ ( "cell", True )
+            , ( "flex", True )
+            , ( "static", cell.cellType == StaticCell )
+            , ( "cell-left", isLeft )
+            , ( "cell-right", isRight )
+            , ( "cell-top", isTop )
+            , ( "cell-bottom", isBottom )
             ]
         ]
+        divContents
+
+
+loadingMessage : Html Msg
+loadingMessage =
+    let
+        rowsCreator i =
+            if i /= 4 then
+                div [ class "flex" ] (List.repeat 9 (div [ class "cell flex" ] [ text "" ]))
+
+            else
+                div [ class "flex" ]
+                    [ div [ class "cell flex" ] [ text "" ]
+                    , div [ class "cell flex static" ] [ text "L" ]
+                    , div [ class "cell flex static" ] [ text "O" ]
+                    , div [ class "cell flex static" ] [ text "A" ]
+                    , div [ class "cell flex static" ] [ text "D" ]
+                    , div [ class "cell flex static" ] [ text "I" ]
+                    , div [ class "cell flex static" ] [ text "N" ]
+                    , div [ class "cell flex static" ] [ text "G" ]
+                    , div [ class "cell flex" ] [ text "" ]
+                    ]
+
+        rows =
+            List.range 0 8
+                |> List.map (\i -> rowsCreator i)
+    in
+    div [ class "board" ] rows
